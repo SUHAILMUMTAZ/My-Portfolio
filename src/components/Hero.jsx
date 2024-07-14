8851532366b62806d749dc0f9ace1529fa718b70
Suhail Mumtaz <suhailmumtaz097@gmail.com>
2024-07-14 19:45:41 +0530

    completed portfolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,70 @@
+
+import { motion } from "framer-motion";
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/Profile.png";
+
+const Container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
+const Hero = () => {
+  return (
+    <div className="px-4 md:px-10 border-b border-neutral-900 pb-6 lg:mb-20">
+      <div className = "flex flex-wrap">
+          
+          <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start mx-4 lg:mx-10">
+            <motion.h1
+              variants={Container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-6 text-3xl md:text-6xl lg:text-5xl font-thin tracking-tight lg:mt-16"
+            >
+              Suhail Mumtaz
+            </motion.h1>
+
+            <motion.span
+              variants={Container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-xl md:text-2xl lg:text-3xl tracking-tight text-transparent"
+            >
+              Full Stack Developer
+            </motion.span>
+
+            <motion.p
+              variants={Container(1)}
+              initial="hidden"
+              animate="visible"
+              className="text-justify my-4 md:my-6 max-w-full md:max-w-xl lg:max-w-3xl py-4 md:py-6 font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
+        </div>
+
+
+
+<div className="w-full lg:w-1/2 lg:p-8 flex justify-center items-center">
+  <motion.img
+    initial={{ x: 100, opacity: 0 }}
+    animate={{ x: 0, opacity: 1 }}
+    transition={{ duration: 1, delay: 1.2 }}
+    className="rounded-[250px] w-48 md:w-64"
+    src={profilePic}
+    alt="Rahi Reyaz"
+  />
+</div>
+
+
+   </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+
+import logo from '../assets/Logo.png';
+import { FaLinkedin, FaGithub, FaPhoneSquare } from 'react-icons/fa';
+import { BiMailSend } from 'react-icons/bi';
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between mx-4 lg:mx-10 py-4">
+      {/* Logo section */}
+      <div className="flex items-center">
+        <img className="w-20 h-auto" src={logo} alt="logo" />
+      </div>
+
+      {/* Links section */}
+      <div className="flex items-center space-x-4 text-xl lg:text-2xl">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/SUHAIL-MUMTAZ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-700"
+        >
+          <FaLinkedin />
+        </a>
+        
+        {/* GitHub */}
+        <a
+          href="https://github.com/SUHAILMUMTAZ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-700"
+        >
+          <FaGithub />
+        </a>
+        
+        {/* Email */}
+        <a
+          href="mailto:example@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-700"
+        >
+          <BiMailSend />
+        </a>
+        
+        {/* Phone */}
+        <a
+          href="tel:+91 9622914972"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-700"
+        >
+          <FaPhoneSquare />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
